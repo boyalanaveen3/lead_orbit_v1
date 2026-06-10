1b31c5e5af5f3e702ff16b9d6695e551289e8f02
naveenboyala <boyalanavee103@gmail.com>
2026-06-10 18:24:52 +0530

    chnaged url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "http://localhost:8787/api"
+    baseURL: "https://leadorbit.boyalanavee103.workers.dev/api"
 })
 
 api.interceptors.request.use((config) => {
